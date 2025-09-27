73a8838b4108804229618ddcecb075ee92cda1be
Choquet Olivier <olivier.choquet@vinci.be>
2025-09-27 08:46:50 +0200

    init repo

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+// src/index.ts
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Hello TypeScript + Express!');
+});
+app.get('/', (_req, res) => {
+  res.send('Hello tt le monde');
+});
+
+//test 
+
+app.get('/dev1', (_req, res) => {
+  res.send('Hello Dev1!');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
+
+export default app;
