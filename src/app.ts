5b6c643a7b6e611b13aa22a6a1ea904fb5f692d8
Choquet Olivier <olivier.choquet@vinci.be>
2025-10-06 08:34:17 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 // import dependencies
-const cors = require('cors');
 import express from "express";
 
 import {tasksController} from './controllers/tasks.controller';
 
 // creates an express app
 export const app = express();
-app.use(cors());
 app.use(express.json());
 
 app.use(express.json());
